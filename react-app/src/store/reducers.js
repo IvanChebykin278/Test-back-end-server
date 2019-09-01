d1762a1196214114a0c9486ad313e76919c0be8b
Chebykin Ivan <chebykin.i.v@gmail.com>
2019-09-01 21:14:37 +0500

    first commit

--- react-app/src/store/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { entryFormReducer } from './entryForm/reducers';
-import { tableReducer } from "./table/reducers";
-
-
-export default combineReducers({
-    entryForm: entryFormReducer,
-    table: tableReducer
-});
